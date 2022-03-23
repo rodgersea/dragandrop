00679f2e27e7fd9d5eb584c20885c6aa2c4ebdd0
rodgersea <elliottarodgers@gmail.com>
2022-03-23 19:27:25 -0400

    local test

--- app.py
+++ app.py
@@ -1,21 +1,17 @@
-from flask import Flask, render_template, request
-from werkzeug import secure_filename
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def upload_file():
+def index():
     return render_template('index.html')
 
 
-@app.route('/uploader', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        f = request.files['file']
-        f.save(secure_filename(f.filename))
-        return 'file uploaded successfully'
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save(uploaded_file.filename)
+    return redirect(url_for('index'))
 
-
-if __name__ == '__main__':
-    app.run(debug=True)
